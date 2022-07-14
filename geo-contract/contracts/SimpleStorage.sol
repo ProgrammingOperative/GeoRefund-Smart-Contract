16a543f27ae4d52d5570c03e6436cccb70fcc465
waCira <wachura11t@gmail.com>
2022-07-14 08:19:17 +0300

    Create first contract

---
+++ geo-contract/contracts/SimpleStorage.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+
+contract SimpleStorage {
+    // declare a state variable called storedData of type uint of 256 bits
+    uint256 storedData;
+
+    }
