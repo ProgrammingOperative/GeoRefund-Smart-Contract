5f0f028672c24898e460f29fb2b05e2b9ff896cb
waCira <wachura11t@gmail.com>
2022-07-17 10:16:51 +0300

    Contruct Complete !

--- geo-contract/contracts/SimpleStorage.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.4.22 <0.9.0;
-
-contract SimpleStorage {
-    // declare a state variable called storedData of type uint of 256 bits
-    uint256 storedData;
-
-    }
